5d60a199fa151bc5c36382e37169ce94f541f927
Geoff G <Geoff17391@gmail.com>
2023-12-15 11:29:47 +0000

    Added Validation on Submit
    + Form validates based on an array of Criteria objects
    + Every field (except message for now) have regex patterns to match
    + Error and Success styles are applied on submit

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,18 +1,114 @@
-// const formElements = {
-//     'first-name': $('#first-name'),
-//     'last-name': $('#last-name'),
-//     'email': $('#email'),
-//     'subject': $('#subject'),
-//     'message': $('#message')
-// };
+const inputCriteria = {
+    "first-name":{
+        min: 3,
+        max: 25,
+        isRequired: true,
+        regex: /^[a-zA-Z]+$/
+    },
+    "last-name":{
+        min: 3,
+        max: 25,
+        isRequired: true,
+        regex: /^[a-zA-Z]+$/
+    },
+    "email":{
+        min: 3,
+        max: 50,
+        isRequired: true,
+        regex: /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/
+    },
+    "subject":{
+        min: 3,
+        max: 25,
+        isRequired: true,
+        regex: /^[0-9a-zA-Z]+$/
+    },
+    "message":{
+        min: 3,
+        max: 9999,
+        isRequired: true
+    }
+};
+
+function checkError(id, inputValue) {
+    //  Get the criteria for the input's validation
+    const currCriteria = inputCriteria[id];
+
+    //  Check if required
+    if(currCriteria.isRequired) {
+        //      Check if below minimum or above maximum
+        if(inputValue.length < currCriteria.min) {
+            return {
+                hasError: true,
+                errorMessage: `<strong>ERROR:</strong> This must be more than ${currCriteria.min} characters long!`
+            }
+        } else if (inputValue.length > currCriteria.max) {
+            return {
+                hasError: true,
+                errorMessage: `<strong>ERROR:</strong> This must be below ${currCriteria.max} characters!`
+            }
+        }
+        if(!inputValue.match(currCriteria.regex)){
+            return {
+                hasError: true,
+                errorMessage: `<strong>ERROR:</strong> Invalid ${id} entered!`
+            }
+        }
+    }
+    return {
+        hasError: false
+    }
+}
+
+function displayError(inputGroup, message) {
+    //  Get the input group's label element
+    const inputLabel = inputGroup.children('label');
+    
+    //  If the success styles are applied, remove them
+    if(inputGroup.hasClass('success'))
+        inputGroup.removeClass('success');
+
+    //  Add the error class to the input group, only if it hasn't already.
+    if(!inputGroup.hasClass('error'))
+        inputGroup.addClass('error');
+
+    //  Add or change the error message.
+    let hasSmall = inputLabel.has('small');
+    if(hasSmall.length === 0)
+        inputLabel.append(` <small>${message}</small>`);
+    else
+        inputLabel.children('small').html(message);
+}
+
+function clearError(inputGroup) {
+    if(inputGroup.hasClass('error'))
+        inputGroup.removeClass('error');
+        inputGroup.children('label').children('small').html('');
+}
+
+function displaySuccess(inputGroup) {
+    if(!inputGroup.hasClass('success'))
+        inputGroup.addClass('success');
+}
 
 $('.form-contact #submit').on('click', function(event){
     event.preventDefault();
-    $('.form-input').each(function(index, element){
-        if($(this).val() === ""){
-            $(this).css('border', '1px solid red');
-            $(this).prev().append(` <small class="error">- must not be empty</small>`);
+    
+    //  For each input, show an error if they're empty
+    $('.form-input').each(function() {
+        const validInput = checkError($(this).attr('id'), $(this).val());
+        if(validInput.hasError) {
+            //  Apply styles
+            displayError($(this).parent(), validInput.errorMessage);
         }
     });
-    // $(this).css('background-color', 'green');
+
+});
+
+$('.form-contact #reset').on('click', function(event) {
+    event.preventDefault();
+    //  For each input, show an error if they're empty
+    $('.form-input').each(function(index, element){
+        clearError($(this).parent());
+    });
 });
