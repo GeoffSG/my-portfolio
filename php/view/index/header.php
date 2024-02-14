1862b531d876b5ba17632a0df74f9c6dee2fe5d9
Geoff G <Geoff17391@gmail.com>
2024-02-14 10:46:09 +0000

    Initial steps to converting HTML to PHP
    The index page is divided into separate PHP files.

---
+++ php/view/index/header.php
@@ -0,0 +1,13 @@
+<div class="wrapper">
+    <header>
+        <section class="cover vh-100">
+            <a href="./index.html">
+                <img src="img/me.png" alt="Photo of Geoffrey Grundell">
+            </a>
+            <h1>
+                <span class="text-effect" id="myName">Geoffrey Grundell</span><br>
+                <small><span class="text-effect" id="myRole">Web Developer</span>
+                    <br><span class="text-effect" id="mySkills">HTML CSS &amp; More</span></small>
+            </h1>
+        </section>
+    </header>
---
+++ php/view/scripts.php
@@ -0,0 +1,5 @@
+<!-- scripts -->
+<script src="js/jquery/jquery.min.js"></script>
+<script src="js/main.js"></script>
+<script src="js/text-effect.js"></script>
+<script async data-id="five-server" src="http://localhost:5500/fiveserver.js"></script>
