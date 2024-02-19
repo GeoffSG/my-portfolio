7b83d846a7532e0c841f94643debb65786ce2ce0
Geoff G <Geoff17391@gmail.com>
2024-02-19 14:08:36 +0000

    Removed "about me" button from footer

--- php/view/footer.php
+++ php/view/footer.php
@@ -1,6 +1,5 @@
 <footer>
     <div class="cover">
-        <a class="btn theme-gold rounded" href="about-me.php">Learn more About Me</a>
         <div class="attribution">
             Website developed by<br>
             <a href="index.html">Geoffrey Grundell</a>
